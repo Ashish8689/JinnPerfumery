325a0a665b0b3d5e56dd2002c28ad2aa68eb8647
Ashish Gupta <ag939431@gmail.com>
2025-12-12 01:10:35 +0530

    prettier fix for navbar component

--- src/component/navbar/Navbar.component.tsx
+++ src/component/navbar/Navbar.component.tsx
@@ -1,120 +1,107 @@
-'use client'
+"use client";
 
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
-import Link from 'next/link'
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import Link from "next/link";
 import {
-    NAV_LINKS,
-    REGEX_SCROLL,
-    SOCIAL_LINKS,
-} from '@/constant/links.constant'
-import GithubIcon from '../../assets/svg/github.svg'
-import './navbar.css'
+  NAV_LINKS,
+  REGEX_SCROLL,
+  SOCIAL_LINKS,
+} from "@/constant/links.constant";
+import GithubIcon from "../../assets/svg/github.svg";
+import "./navbar.css";
 
-
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 
 const Navbar: React.FC = () => {
-    const [isPageScrolling, setIsPageScrolling] = useState(false)
-    const [isHamburgerOpen, setIsHamburgerOpen] = useState(false)
+  const [isPageScrolling, setIsPageScrolling] = useState(false);
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
 
-    const pathname = usePathname()
+  const pathname = usePathname();
 
-    const isLandingPage = useMemo(() => pathname === '/', [pathname])
+  const isLandingPage = useMemo(() => pathname === "/", [pathname]);
 
-    const changeBackground = useCallback((): void => {
-        if (window.scrollY >= 100) {
-            setIsPageScrolling(true)
-        } else {
-            setIsPageScrolling(false)
-        }
-    }, [])
-
-    const handleHamburger = (): void => setIsHamburgerOpen(!isHamburgerOpen)
-
-    const handleNavLinkClick = (
-        e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-    ): void => {
-        setIsHamburgerOpen(false)
-
-        if (isLandingPage) {
-            // first prevent the default behavior
-            e.preventDefault()
-            // get the href and remove everything before the hash (#)
-            const href = e.currentTarget.href
-            const targetId = href.replace(REGEX_SCROLL, '')
-            // get the element by id and use scrollIntoView
-            const elem = document.getElementById(targetId)
-            elem?.scrollIntoView({
-                behavior: 'smooth',
-            })
-        }
+  const changeBackground = useCallback((): void => {
+    if (window.scrollY >= 100) {
+      setIsPageScrolling(true);
+    } else {
+      setIsPageScrolling(false);
     }
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener('scroll', changeBackground)
-    }, [changeBackground])
+  const handleHamburger = (): void => setIsHamburgerOpen(!isHamburgerOpen);
 
-    return (
-        <div className={isPageScrolling ? 'nav active' : 'nav'}>
-            <div className="container">
-                <div className="logo-block">
-                    <Link href="/">
-                        <h1 className="logo">Ashish</h1>
-                    </Link>
-                </div>
+  const handleNavLinkClick = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ): void => {
+    setIsHamburgerOpen(false);
 
-                <div className="side-menu">
-                    <ul
-                        className={
-                            isHamburgerOpen ? 'nav-list active' : 'nav-list'
-                        }
-                    >
-                        {NAV_LINKS.map(({ page, link }) => (
-                            <Link
-                                className="nav-link"
-                                href={isLandingPage ? link : ''}
-                                key={page}
-                                onClick={handleNavLinkClick}
-                            >
-                                {page}
-                            </Link>
-                        ))}
+    if (isLandingPage) {
+      // first prevent the default behavior
+      e.preventDefault();
+      // get the href and remove everything before the hash (#)
+      const href = e.currentTarget.href;
+      const targetId = href.replace(REGEX_SCROLL, "");
+      // get the element by id and use scrollIntoView
+      const elem = document.getElementById(targetId);
+      elem?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  };
 
-                        <Link
-                            className="nav-link"
-                            href="open-source"
-                            prefetch={false}
-                        >
-                            OpenSource
-                        </Link>
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+  }, [changeBackground]);
 
-                        <Link
-                            className="nav-link"
-                            href={SOCIAL_LINKS.GITHUB}
-                            target="_blank"
-                        >
-                            <GithubIcon className="github" />
-                        </Link>
-                    </ul>
-                </div>
-
-                <div className="hamburger" onClick={handleHamburger}>
-                    <span
-                        className={
-                            isHamburgerOpen ? 'span span1 active' : 'span span1'
-                        }
-                    />
-                    <span
-                        className={
-                            isHamburgerOpen ? 'span span2 active' : 'span span2'
-                        }
-                    />
-                </div>
-                <div className="box-bar" />
-                <div className="circle-round" />
-            </div>
+  return (
+    <div className={isPageScrolling ? "nav active" : "nav"}>
+      <div className="container">
+        <div className="logo-block">
+          <Link href="/">
+            <h1 className="logo">Ashish</h1>
+          </Link>
         </div>
-    )
-}
 
-export default Navbar
+        <div className="side-menu">
+          <ul className={isHamburgerOpen ? "nav-list active" : "nav-list"}>
+            {NAV_LINKS.map(({ page, link }) => (
+              <Link
+                className="nav-link"
+                href={isLandingPage ? link : ""}
+                key={page}
+                onClick={handleNavLinkClick}
+              >
+                {page}
+              </Link>
+            ))}
+
+            <Link className="nav-link" href="open-source" prefetch={false}>
+              OpenSource
+            </Link>
+
+            <Link
+              className="nav-link"
+              href={SOCIAL_LINKS.GITHUB}
+              target="_blank"
+            >
+              <GithubIcon className="github" />
+            </Link>
+          </ul>
+        </div>
+
+        <div className="hamburger" onClick={handleHamburger}>
+          <span
+            className={isHamburgerOpen ? "span span1 active" : "span span1"}
+          />
+          <span
+            className={isHamburgerOpen ? "span span2 active" : "span span2"}
+          />
+        </div>
+        <div className="box-bar" />
+        <div className="circle-round" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
