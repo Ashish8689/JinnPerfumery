61b4ceb6644d1c1c3a6ac0acd5a326b6c55d08f7
Ashish Gupta <ag939431@gmail.com>
2025-12-16 23:35:06 +0530

    remove topbanner and instagram section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import BestSeller from '@/component/BestSeller/BestSeller.component'
 import HeroCarousel from '@/component/HeroCarousel/HeroCarousel.component'
-import InstagramSection from '@/component/InstagramSection/InstagramSection.component'
 import LatestArrivals from '@/component/LatestArrivals/LatestArrivals.component'
 import ShopByCategory from '@/component/ShopByCategory/ShopByCategory.component'
 import Testimonials from '@/component/Testimonials/Testimonials.component'
-import TopBanner from '@/component/TopBanner/TopBanner.component'
 import WhyChooseUs from '@/component/WhyChooseUs/WhyChooseUs.component'
 import WhyTrustUs from '@/component/WhyTrustUs/WhyTrustUs.component'
 
@@ -12,14 +10,12 @@ export default function Home(): React.JSX.Element {
     return (
         <div className="main">
             <HeroCarousel />
-            <TopBanner />
             <LatestArrivals />
             <WhyChooseUs />
             <BestSeller />
             <ShopByCategory />
             <WhyTrustUs />
             <Testimonials />
-            <InstagramSection />
         </div>
     )
 }
